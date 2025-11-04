d6d22dab87fbd47a88fc01235e9a756f8a91715f
Bonnier Nilss Rodrigo <61536276@upeu.edu.pe>
2025-11-03 21:41:15 -0500

    first commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,55 @@
-import { Component, signal } from '@angular/core';
+import {AfterViewInit, Component, OnInit, signal} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {Persona} from './models/Persona';
+import {NgForOf} from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgForOf],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
-export class App {
+export class App implements OnInit, AfterViewInit {
+  //Vamos a crear un objeto
+  public persona : Persona = new Persona(); //Esta es una forma de instanciar un objeto
+  public nombre: string = "";
+  public personas: Persona[] = []; //Vamos a crear una lista. Se a instanciado de tipo lista
   protected readonly title = signal('lista_games');
+  uniquekey: any;
+
+  ngAfterViewInit(): void {
+    console.log("ngAfterViewInit");
+  }
+
+  ngOnInit(): void {
+    this.nombre = "Hombre lobo de mar 2";
+    console.log("ngOnInit()");
+    console.log(this.nombre);
+    this.persona.nombre = 'Rodrigito 1'
+    this.persona.email = 'rodrigito@gmail.com'
+    this.persona.phone = '999999999'
+    this.persona.addres = 'Sin dirección'
+    this.personas.push(this.persona);
+    this.personas.push(this.persona);
+    this.personas.push(this.persona);
+    this.personas.push(this.persona);
+    this.personas.push(this.persona);
+    console.log(this.personas)
+    console.log(this.persona);
+    }
+  constructor() {
+    console.log(this.nombre);
+    console.log("constructor")
+    this.nombre = "Hombre lobo de mar";
+    console.log(this.nombre);
+  }
+
+  clickMe(persona: Persona): void {
+    alert(JSON.stringify(persona));
+    // Cuando ejecuto ese metodo, va votar un alert y me va a mostrar esa información de ese objeto
+  }
+
+  agregar(persona: Persona) {
+    this.personas.push(this.persona);
+  }
 }
---
+++ src/app/models/Persona.ts
@@ -0,0 +1,8 @@
+export class Persona {
+  nombre?: string;
+  email?: string;
+  phone?: string;
+  addres?: string;
+}
+//Nos pide el constructor, podemos resolverlo poniendo ?, para que solo reciba vacio
+
